1944b161eef183de2f67d1d1cc3260015ec8ffa5
Darwin Verastegui <darwinjverasteguis2002@gmail.com>
2024-04-24 00:15:42 +0000

    async funct added and issue resolved with the posting of the data

--- routes/api.js
+++ routes/api.js
@@ -6,49 +6,46 @@ const BoardModel = require('../models').Board
 
 module.exports = function (app) {
   
-  app.route('/api/threads/:board').post((req, res) => {
-    const { text, delete_password } = req.body
-    let board = req.body.board
-    if(!board){
-      board = req.params.board
+  app.route('/api/threads/:board').post(async (req, res) => {
+    const { text, delete_password } = req.body;
+    let board = req.body.board;
+    if (!board) {
+      board = req.params.board;
     }
-    console.log('post', req.body)
-    const newThread = new ThreadModel({
-      text: text,
-      delete_password: delete_password,
-      replies: [],
-    })
-    console.log('newThread', newThread)
-    BoardModel.findOne({ name: board}, (err, BoardData) => {
-      if(!BoardData){
+  
+    try {
+      console.log('post', req.body);
+      const newThread = new ThreadModel({
+        text: text,
+        delete_password: delete_password,
+        replies: [],
+      });
+      console.log('newThread', newThread);
+  
+      let boardData = await BoardModel.findOne({ name: board });
+      if (!boardData) {
         const newBoard = new BoardModel({
           name: board,
-          threads: [], 
-        })
-        console.log('newBoard', newBoard)
-        newBoard.threads.push(newThread)
-        newBoard.save((err, data) => {
-        console.log('newBoardData', data)
-        if(err || data) {
-          console.log(err)
-          res.send("there was an error saving in post")
-        } else {
-          res.json(newThread)
-        }
-      })
+          threads: [newThread], 
+        });
+        console.log('newBoard', newBoard);
+        await newBoard.save();
+        console.log('newBoardData', newBoard);
+        res.json(newThread);
       } else {
-        BoardData.threads.push(newThread)
-        BoardData.save((err, data) => {
-          if(err || data) {
-            console.log(err)
-            res.send("there was an error saving in post")
-          } else {
-            res.json(newThread)
-          }
-        })
+        boardData.threads.push(newThread);
+        await boardData.save();
+        res.json(newThread);
       }
-    })
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("There was an error saving in post");
+    }
   })
+  .get((req, res) => {
+    
+  })
+  
     
   app.route('/api/replies/:board');
 
