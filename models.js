a7aa6315a02f73d071ec8a92b17fb9a195a6cb17
Darwin Verastegui <darwinjverasteguis2002@gmail.com>
2024-04-25 04:18:51 +0000

    models and schemas added to the inicial state, looking forward to fix bugs and end the project soon

--- models.js
+++ models.js
@@ -4,29 +4,29 @@ const { Schema } = mongoose
 const date = new Date()
 
 const ReplySchema = new Schema({
-    text: {type: String},
-    delete_password: {type: String},
-    create_on: {type: Date, default: date},
-    bumped_on: {type: Date, default: date},
-    reported: {type: Boolean, default: false},
+  text: {type: String, required: true},
+  delete_password: {type: String, required: true},
+  created_on: {type: Date, default: date},
+  bumped_on: {type: Date, default: date},
+  reported: {type: Boolean, default: false}
 })
 
 const Reply = mongoose.model('Reply', ReplySchema)
 
 const ThreadSchema = new Schema({
-    text: {type: String},
-    delete_password: {type: String},
-    reported: {type: Boolean, default: false},
-    created_on: {type: Date, default: date},
-    bumped_on: {type: Date, default: date},
-    replies: {type: [ReplySchema]},
+  text: {type: String, required: true},
+  delete_password: {type: String, required: true},
+  created_on: {type: Date, default: date},
+  bumped_on: {type: Date, default: date},
+  reported: {type: Boolean, default: false},
+  replies: [ReplySchema]
 })
 
 const Thread = mongoose.model('Thread', ThreadSchema)
 
 const BoardSchema = new Schema({
-    name: {type: String},
-    threads: {type: [ThreadSchema]},
+  name: {type: String, require: true},
+  threads: [ThreadSchema]
 })
 
 const Board = mongoose.model('Board', BoardSchema)
